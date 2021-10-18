eb6c5890ce631f5371cb3716719aab17cbcedd91
MissLitaa <anahug49@gmail.com>
2021-10-18 12:49:35 +0200

    Entregable 2
    2

---
+++ Assets/Script/playerHP.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerHP : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    //La variable indica si es Game Over
+    [SerializeField] private bool isGameOver;
+
+    //La variable indica la vida del player
+    private int playerHP = 100;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
