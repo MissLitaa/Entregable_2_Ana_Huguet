eeb0f0fb161edf240673b91a586842597151758f
MissLitaa <anahug49@gmail.com>
2021-10-18 18:30:21 +0200

    Entregable2
    2

--- Assets/Script/playerHP.cs
+++ Assets/Script/playerHP.cs
@@ -6,19 +6,47 @@ public class playerHP : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    //La variable indica si es Game Over
+    //La variable indica si es Game Over.
     [SerializeField] private bool isGameOver;
 
-    //La variable indica la vida del player
-    private int playerHP = 100;
+    //La variable indica la vida del player.
+    private int PlayerHP = 100;
+
+    //La variable indica el daño recibido por el player.
+    public int Damage = 30;
+
+
     void Start()
     {
-        
+        Debug.Log($"Player HP: {PlayerHP -= Damage}");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (isGameOver == false)
+
+        {
+            if (PlayerHP <= 0)
+            {
+                Debug.Log("Te has quedado sin vida...GAME OVER");
+            }
+
+            else if (PlayerHP < 20)
+            {
+                Debug.Log("¡Tienes poca vida!");
+            }
+
+            else
+            {
+                Debug.Log($"Vas bien de vida; tienes  {PlayerHP} puntos de vida.");
+            }
+        }
+
+        if (isGameOver == true)
+        {
+            Debug.Log("GAME OVER");
+        }
     }
 }
